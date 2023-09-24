717689e31a22714474fa395cfaa882bf5e58e381
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2023-09-24 20:20:30 +0300

    added card

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,13 +1,26 @@
 import styles from "./Menu.module.css";
 import Heading from "../../components/Heading/Heading";
 import Search from "../../components/Search/Search";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 const Menu = () => {
   return (
-    <div className={styles["header"]}>
-      <Heading title='Menu' />
-      <Search placeholder='Search product' />
-    </div>
+    <>
+      <div className={styles["header"]}>
+        <Heading title='Menu' />
+        <Search placeholder='Search product' />
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title='Наслаждение'
+          description='Салями, руккола, помидоры, оливки'
+          rating={4.5}
+          price={300}
+          image='/product-demo.png'
+        />
+      </div>
+    </>
   );
 };
 
